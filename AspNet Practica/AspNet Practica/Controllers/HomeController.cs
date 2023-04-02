7547147e65d3e627a23fc21f512fc6d5173c9fc1
Mirze26 <mirzaab@code.edu.az>
2023-04-03 02:59:33 +0400

    Aspnet

---
+++ AspNet Practica/AspNet Practica/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_Practica.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            int[] nums1 = { 1, 4, 7, 8, 9, 0, 5 };
+            int[] nums2 = { 15, 4, 7, 8, 9, 30, 15 };
+
+            
+
+            return View(nums1);
+        }
+
+
+
+
+        //public string Index(int id)
+        //{
+        //    if (id == 2)
+        //    {
+        //        return "Anar";
+        //    }
+            
+        //    return "Aqshin - " + id ;
+            
+        //}
+
+
+
+        //public string Detail (string slug, string name)
+        //{
+        //    return slug + name;
+        //}
+
+
+
+         
+        //private string GetName()
+        //{
+        //    return "Aqshin";
+        //}
+
+
+
+
+    }
+
+
+    
+}
